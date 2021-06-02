ebdbc2f97e1c6c8f924cf3deffba5e1e350333da
sonicxz <abhionic@outlook.com>
2021-06-02 21:01:51 +0530

    UI for customer bids refactored and fixed. The UI now implements a MUI Table with proper spacing

--- src/containers/CustomerBids/CustomerBids.tsx
+++ src/containers/CustomerBids/CustomerBids.tsx
@@ -1,4 +1,4 @@
-import { RouteComponentProps } from '@reach/router';
+import { navigate, RouteComponentProps } from '@reach/router';
 import React from 'react';
 import { useCustomerContext } from '../../hooks';
 import {
@@ -11,19 +11,47 @@ import {
 	TableRow,
 	TableHead,
 	Avatar,
-	TableFooter,
 	Box,
 } from '@material-ui/core';
+import { Routes } from '../../repos';
 
 interface CustomerBidsProps extends RouteComponentProps {}
 const CustomerBids = (props: CustomerBidsProps) => {
 	const { customer } = useCustomerContext();
+
+	if (!customer) {
+		alert('Please select a customer');
+		navigate(Routes.Home);
+		return null;
+	}
+
 	return (
-		<Box display="flex" justifyContent="center" mt={8}>
-			<Avatar src={customer?.avatarUrl}></Avatar>
-			<h1>{customer?.firstname}</h1>
+		<Box display="flex" justifyContent="center" mt={8} flexDirection="column">
+			<Box display="flex" alignItems="center">
+				<Avatar src={customer.avatarUrl} style={{ margin: '1rem' }}></Avatar>
+				<h1>{`${customer.firstname} ${customer.lastname}`}</h1>
+			</Box>
 			<TableContainer component={Paper}>
-				<MuiTable></MuiTable>
+				<MuiTable>
+					<TableHead>
+						<TableRow>
+							<TableCell>Car Title</TableCell>
+							<TableCell>Amount</TableCell>
+							<TableCell>Created At</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{customer.bids.map((bid) => {
+							return (
+								<TableRow key={bid.id}>
+									<TableCell>{bid.carTitle}</TableCell>
+									<TableCell>{bid.amount}</TableCell>
+									<TableCell>{bid.created}</TableCell>
+								</TableRow>
+							);
+						})}
+					</TableBody>
+				</MuiTable>
 			</TableContainer>
 		</Box>
 	);
